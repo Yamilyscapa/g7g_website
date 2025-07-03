98939aa4d0e795b52b5cbc1fcc1447a07166efce
yam <yamil.yscapa@gmail.com>
2025-07-02 19:12:34 -0600

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // No integrations needed for Sass, just install 'sass' and use <style lang="scss"> in your components.
+});
